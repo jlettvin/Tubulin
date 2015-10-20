d4ff00ab8430e421fac04c3be0bbb5c28121038b
Jonathan D. Lettvin <jlettvin@gmail.com>
2015-10-19 22:17:55 -0400

    make ring spines G, others 7

--- unique.path.py
+++ unique.path.py
@@ -164,7 +164,7 @@ class Tree(object):
         x, y, z = point
         N = sqrt(x**2+y**2)
 
-        radius, margin = 0.75, 0.04
+        radius, margin = 0.75, 0.035
         lower, upper = radius - margin, radius + margin
         active = (N > lower and N < upper)
 
